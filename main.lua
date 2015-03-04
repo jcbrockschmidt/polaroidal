6967b2ff820820b04008b19bad53c2217f7d3c1b
JoshuaBrockschmidt <JoshuaBrockschmidt@gmail.com>
2015-03-04 12:19:15 -0800

    Added parameters for polar equations.

--- main.lua
+++ main.lua
@@ -1,6 +1,12 @@
 function love.load()
    require "polar"
-   newGraph = polar.new(love.graphics.getWidth() / 2, love.graphics.getHeight() / 2, 50)
+   newGraph = polar.new(
+      love.graphics.getWidth() / 2,
+      love.graphics.getHeight() / 2,
+      0,
+      100,
+      0, 1, 5
+   )
 
    love.graphics.setBackgroundColor(255, 255, 255)
 end
@@ -11,5 +17,11 @@ end
 
 function love.draw()
    love.graphics.setColor(0, 0, 0)
+   love.graphics.circle(
+      "line",
+      love.graphics.getWidth() / 2,
+      love.graphics.getHeight() / 2,
+      5
+   )
    newGraph:draw()
 end
--- polar.lua
+++ polar.lua
@@ -1,15 +1,18 @@
-local incr = math.pi / 6
+local incr = math.pi / 64
 local maxPoints = math.floor(2 * math.pi / incr)
+local lineThickness = 4
 
 local graph = {
    --- Calculates cartesian points for a given polarized graph object.
    -- @param self  Polarized graph object.
+   -- @param scale  Number. Scale of graph. Will augment radius.
    calcPoints = function(self)
-      local angle
+      local angle, r
       for p = 1, maxPoints do
 	 angle = incr * p
-	 table.insert(self.points, (self.r * math.cos(angle)) + self.x)
-	 table.insert(self.points, (self.r * math.sin(angle)) + self.y)
+	 r = (self.a + (self.b * math.sin(self.n * (angle + self.rads)))) * self.scale
+	 table.insert(self.points, (r * math.cos(angle)) + self.x)
+	 table.insert(self.points, (r * math.sin(angle)) + self.y)
       end
       table.insert(self.points, self.points[1])
       table.insert(self.points, self.points[2])
@@ -18,6 +21,7 @@ local graph = {
    --- Draws a polarized graph object.
    -- @param self  Polarized graph object.
    draw = function(self)
+      love.graphics.setLineWidth(lineThickness)
       love.graphics.line(unpack(self.points))
    end
 }
@@ -30,19 +34,28 @@ polar = {
    --- Creates a new polarized graph.
    -- @param x  Number. X-coordinate of graph's origin.
    -- @param y  Number. Y-coordinate of graph's origin.
+   -- @param rads  Number [0, 2*pi].
+   -- @param scale  Number. Radius will be augmented to shrunken according to
+   --  this number when graph's points are calculated.
+   -- @param a  Number.
+   -- @param b  Number (0, oo).
+   -- @param n  Number.
    -- @return  A polarized graph object.
-   new = function(x, y, r)
+   new = function(x, y, rads, scale, a, b, n)
       local obj = {
 	 x = x,
 	 y = y,
-	 r = r,
+	 rads = rads or 0,
+	 scale = scale or 1,
+	 a = a or 0,
+	 b = b or 1,
+	 n = n or 1,
 	 points = {}
       }
-      local angle
       setmetatable(obj, mt)
       obj:calcPoints()
       return obj
-   end,
-   
-   draw = graph.draw
+   end
 }
+
+setmetatable(polar, mt)
